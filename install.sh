e4a848bd41e871b3329b46ace1637a3a88b43638
EC2 Default User <ec2-user@ip-10-0-1-40.ap-southeast-1.compute.internal>
2015-12-14 08:26:34 +0000

    adds python 2.6,2.7,3.2,3.3,3.4,3.5 support

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -e
+
+# install python prereqs
+add-apt-repository -y ppa:fkrull/deadsnakes
+apt-get update
+apt-get install -y libxml2 libxml2-dev libxslt1.1 libxslt1-dev libffi-dev libssl-dev libpq-dev
+
+#!/bin/bash
+for file in /u12pytpls/version/*;
+do
+  $file
+done
