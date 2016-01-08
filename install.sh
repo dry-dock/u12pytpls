e152c63147039092e1af1a42bf1f28ac97749815
balavignesh-s <balavignesh@shippable.com>
2016-01-08 15:44:30 +0530

    adding libmysqlclient package

--- install.sh
+++ install.sh
@@ -3,8 +3,10 @@
 # install python prereqs
 add-apt-repository -y ppa:fkrull/deadsnakes
 apt-get update
-apt-get install -y libxml2 libxml2-dev libxslt1.1 libxslt1-dev libffi-dev libssl-dev libpq-dev libmysqlclient-dev
-
+sudo dpkg --remove --force-depends libmysqlclient18
+sudo apt-get install libmysqlclient18
+apt-get install -y libxml2 libxml2-dev libxslt1.1 libxslt1-dev libffi-dev libssl-dev libpq-dev
+sudo apt-get install libmysqlclient-dev
 #!/bin/bash
 for file in /u12pytpls/version/*;
 do
