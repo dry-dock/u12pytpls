7d20e8c21f0d8db676494aec1ee6d561cea444a0
chetantarale <chetan_tarale@yahoo.com>
2016-01-25 17:54:48 +0530

    add missing lines

--- install.sh
+++ install.sh
@@ -3,7 +3,10 @@
 # install python prereqs
 add-apt-repository -y ppa:fkrull/deadsnakes
 apt-get update
-apt-get install -y libxml2 libxml2-dev libxslt1.1 libxslt1-dev libffi-dev libssl-dev
+dpkg --remove --force-depends libmysqlclient18
+apt-get install libmysqlclient18
+apt-get install -y libxml2 libxml2-dev libxslt1.1 libxslt1-dev libffi-dev libssl-dev libpq-dev
+apt-get install libmysqlclient-dev
 
 # Installing pip-7.1.2
 wget https://bootstrap.pypa.io/3.2/get-pip.py
