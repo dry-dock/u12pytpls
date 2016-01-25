f056d212e602306e3787b35404af0ccd7da93619
chetantarale <chetan_tarale@yahoo.com>
2016-01-25 17:42:43 +0530

    wget pip and venv

--- install.sh
+++ install.sh
@@ -3,12 +3,21 @@
 # install python prereqs
 add-apt-repository -y ppa:fkrull/deadsnakes
 apt-get update
-sudo dpkg --remove --force-depends libmysqlclient18
-sudo apt-get install libmysqlclient18
-apt-get install -y libxml2 libxml2-dev libxslt1.1 libxslt1-dev libffi-dev libssl-dev libpq-dev
-sudo apt-get install libmysqlclient-dev
-#!/bin/bash
+apt-get install -y libxml2 libxml2-dev libxslt1.1 libxslt1-dev libffi-dev libssl-dev
+
+# Installing pip-7.1.2
+wget https://bootstrap.pypa.io/3.2/get-pip.py
+python get-pip.py
+
+# Installing virtualenv-13.1.2
+wget https://pypi.python.org/packages/source/v/virtualenv/virtualenv-13.1.2.tar.gz#md5=b989598f068d64b32dead530eb25589a
+tar xvzf virtualenv-13.1.2.tar.gz
+cd virtualenv-13.1.2
+python setup.py install
+cd /
+
 for file in /u12pytpls/version/*;
-do
-  $file
-done
+  do
+    $file
+  done
+
